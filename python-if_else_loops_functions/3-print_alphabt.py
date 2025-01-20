1e8949f3396192f7374549f8eca3aa275a8c3d97
RipsySais <cysseahmed@gmail.com>
2025-01-20 11:02:51 +0000

    3-print

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for char in range(97, 123):
-    if char != 101 and char != 113:
-        print(chr(char), end="")
+    if (char != 101 and char != 113):
+        print("{}".format(chr(char)), end="")
+    continue
