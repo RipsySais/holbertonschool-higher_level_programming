a36a06bef72a12ac14c17985641853eb62687bcf
RipsySais <cysseahmed@gmail.com>
2025-01-31 13:13:32 +0000

    1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+''' this module defines a class Square'''
+
+
+class Square:
+    ''' a square class with a size attribute'''
+    def __init__(self, size):
+        ''' initializes the size attribute'''
+        self.__size = size
