200ffd01122950afef15b09f3d970106406973f1
RipsySais <cysseahmed@gmail.com>
2025-01-31 13:57:44 +0000

    4-square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 
-""" Square class with size attribute"""
+"""This module defines a class Square."""
 
 
 class Square:
-    """ a square class"""
+    """A class that defines a square."""
+
     def __init__(self, size=0):
-        """ initializes the size attribute"""
-        self.__size = size
-        if type(size) is not int:
+        """Initialize the square with a private size attribute."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        """ returns the area of the square"""
+        """Calculate and return the area of the square."""
         return self.__size ** 2
