eb0eb3699d7902def449c53b88639c72beab8d7c
RipsySais <cysseahmed@gmail.com>
2025-01-31 13:50:41 +0000

    4-square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,8 +8,12 @@ class Square:
     def __init__(self, size=0):
         """ initializes the size attribute"""
         self.__size = size
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """ returns the area of the square"""
+        return self.__size ** 2
