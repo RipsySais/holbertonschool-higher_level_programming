4511ca4cb24ab971ed7d9a9bfe87de1e1bc7447a
RipsySais <cysseahmed@gmail.com>
2025-02-03 16:42:15 +0000

    5-base_geo

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class called BaseGeometry.
+Ce module définit une classe appelée BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    an empty class called BaseGeometry
+    Une classe vide appelée BaseGeometry.
+    """
+    pass
