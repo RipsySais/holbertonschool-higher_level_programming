ada08d9f9b8199f787c19ddfcfaed07762720729
RipsySais <cysseahmed@gmail.com>
2025-02-05 20:36:13 +0000

    4-inherits

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+"""Module that return true if
+object is exactly an instance of the specified class
+"""
+
+
 def inherits_from(obj, a_class):
-    """
-    Vérifie si un objet est une instance d'une classe héritée d'une classe
-Args:
-obj (object): L'objet à vérifier
-a_class (class): La classe à vérifier
-Retourne:
-bool: True si obj est une instance d'une classe héritée d'a_class, sino faux
-    """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    """Function that return true if
+    object is exactly an instance of the specified class"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
