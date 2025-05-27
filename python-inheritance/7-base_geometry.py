bc99784ef311a81910f70b7ebfa69c981c78213e
RipsySais <cysseahmed@gmail.com>
2025-05-27 22:28:47 +0000

    base_geometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
-This module defines a class BaseGeometry
-Ce module définit une classe appelée BaseGeometry
+This module defines an abstract class BaseGeometry.
+Ce module définit une classe abstraite BaseGeometry.
 """
 
 
 class BaseGeometry:
     """
-    A class BaseGeometry with a method area()
-    Une classe BaseGeometry avec une méthode area()
+    A class BaseGeometry with a method area() and integer validation.
+    Une classe BaseGeometry avec une méthode area() et une validation d'entier.
     """
 
     def area(self):
         """
         Calculate the area of the geometry.
         Calcule l'aire de la géométrie.
+        Raises:
+            Exception: If the method is not implemented.
         """
         raise Exception("area() is not implemented")
 
@@ -22,8 +24,16 @@ class BaseGeometry:
         """
         Validate the value.
         Valide la valeur.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to zero.
         """
         if not isinstance(value, int):
-            raise TypeError(name + " must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
