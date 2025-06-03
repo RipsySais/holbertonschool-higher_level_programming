c59d57fe6a3544f3983db2a39d88bc235428571c
RipsySais <cysseahmed@gmail.com>
2025-06-03 22:15:49 +0000

    0_read

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
-def read_file(filename=""):
+#!/usr/bin/python3
+"""Read a file and print its contents
+to the standard output."""
 
-    """Reads a file and returns its content."""
-    with open(filename, 'r', encoding='utf-8') as f:
+
+def read_file(filename=""):
+    """
+    Read a file and print its contents
+    """
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
