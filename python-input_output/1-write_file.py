11bbd1b098f5be90e93ad4fd013b76806ba5121d
RipsySais <cysseahmed@gmail.com>
2025-06-03 22:29:49 +0000

    1_write

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bash/python3
+def write_file(filename="", text=""):
+    """
+    Write a string to a text file and return the number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
