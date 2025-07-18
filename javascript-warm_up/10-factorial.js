b88e9a872b86032271c1c974b83b56ed8db43e33
RipsySais <cysseahmed@gmail.com>
2025-07-18 22:54:12 +0000

    task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// Calcul récursif de factorielle
+
+function factorial (n) {
+  if (isNaN(n)) return 1;
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
