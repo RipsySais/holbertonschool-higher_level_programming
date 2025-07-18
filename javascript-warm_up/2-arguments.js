d9e1a17afbcde47c1654dab2fd130447f43f9cf6
RipsySais <cysseahmed@gmail.com>
2025-07-18 22:00:41 +0000

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -6,16 +6,13 @@
 // - process.argv[0] : le chemin vers l'exécutable Node.js
 // - process.argv[1] : le chemin vers le script en cours d'exécution
 // - process.argv[2...] : les arguments passés par l'utilisateur
+
 const args = process.argv.slice(2);
 
-// Vérifie le nombre d'arguments passés
 if (args.length === 0) {
-    // Si aucun argument n'est passé
-    console.log("No argument");
+  console.log('No argument');
 } else if (args.length === 1) {
-    // Si un seul argument est passé
-    console.log("Argument found");
+  console.log('Argument found');
 } else {
-    // Si plusieurs arguments sont passés
-    console.log("Arguments found");
+  console.log('Arguments found');
 }
