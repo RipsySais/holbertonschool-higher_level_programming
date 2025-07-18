58aed044f7514e1e7f861b33c6aeeb31c9094af6
RipsySais <cysseahmed@gmail.com>
2025-07-18 22:21:41 +0000

    task 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+// Script qui affiche 3 langages en utilisant un tableau et une boucle
+
+const languages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (const lang of languages) {
+  console.log(lang);
+}
