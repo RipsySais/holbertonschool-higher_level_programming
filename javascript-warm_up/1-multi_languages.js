f620ce3256cff214bda2f37a86cd9f7a1628047b
RipsySais <cysseahmed@gmail.com>
2025-07-18 21:42:57 +0000

    Add 1-multi_languages.js: print 3 lines in different languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+/*
+  Script : 1-multi_languages.js
+  Objectif : Afficher 3 lignes dans la console, chacune représentant
+             une phrase dans un langage de programmation différent.
+  Contraintes :
+    - Utiliser `console.log(...)` pour chaque ligne
+    - Ne pas utiliser `var`, uniquement `const` ou `let`
+    - Respecter la norme semistandard
+*/
+
+console.log('C is fun'); // Affiche une phrase sur le langage C
+console.log('Python is cool'); // Affiche une phrase sur Python
+console.log('JavaScript is amazing'); // Affiche une phrase sur JavaScript
