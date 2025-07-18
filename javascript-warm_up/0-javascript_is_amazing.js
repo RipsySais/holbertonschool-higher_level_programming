599f03221bbfa18f956ce687eaa208581d88b89a
RipsySais <cysseahmed@gmail.com>
2025-07-18 21:26:32 +0000

    Add 0-javascript_is_amazing.js: print a constant

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+  Script : 0-javascript_is_amazing.js
+  Objectif : Déclarer une constante contenant une chaîne de caractères
+             et l'afficher dans la console.
+  Contraintes :
+    - Utiliser une constante (pas de `var`)
+    - Utiliser `console.log`
+    - Respecter la norme semistandard
+*/
+
+const myVar = 'JavaScript is amazing'; // Déclaration d'une constante avec le texte demandé
+console.log(myVar); // Affiche le contenu de la constante dans la console
