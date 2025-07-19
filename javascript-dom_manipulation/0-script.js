61d1735f068477a2d80587288672843c0b078484
RipsySais <cysseahmed@gmail.com>
2025-07-19 00:53:54 +0000

    task 0

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,7 @@
+// Sélectionne le premier élément header trouvé dans le document
+const headerElement = document.querySelector('header');
+
+// Change la couleur du texte en rouge (#FF0000)
+if (headerElement) {
+  headerElement.style.color = '#FF0000';
+}
